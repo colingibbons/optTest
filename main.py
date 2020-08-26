1522ecfc502539f55ef183294e78706070986c74
cogibbo <cogibbo@siue.edu>
2020-08-26 12:36:43 -0500

    added comparison ED/SED functions

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
 import numpy as np
-import scipy.optimize as opt
-import matplotlib.pyplot as plt
+# import scipy.optimize as opt
+import cvxopt as cvx
+import testFunctions as test
 from timer import Timer
-
-
-# define a test function
-def testFunction(x, y):
-    output = (x**2) + (y**2)
-    return output
 
 # initialize timer object
 t = Timer()
 
-# initialize plot
-fig = plt.figure()
-ax = plt.axes(projection='3d')
+# define necessary variables
+x = []
+y = []
+x.append([100, 100])
+y.append([100, 300])
 
-# generate test data set
-x = np.linspace(-10, 10, 21)
-y = np.linspace(-10, 10, 21)
-X, Y = np.meshgrid(x, y)
-Z = testFunction(X, Y)
+# start the timer
+t.start()
 
-# display basic statistics for data set
-print("Test data generation complete. Here's some stats about the data:")
-print("Number of points: " + str(len(Z)))
-print("Minimum Value: " + str(np.min(Z)))
-print("Maximum Value: " + str(np.max(Z)))
-print("Average Value: " + str(np.mean(Z)))
+# code to best tested goes here
+resultOne = test.squaredEuclideanOne(x, y)
+print(resultOne)
 
-# generate plot for data set
-ax.plot_surface(X, Y, Z)
+#stop the timer
+t.stop()
 
-# title and axis labels
-ax.set_title('3D Plot of Objective Function')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+# start a second timer
+t.start()
 
-# display the plot
-plt.show()
-# define optimization object
-#res = opt.minimize(testFunction, x, y, method='nelder-mead', options={'xatol': 1e-8, 'disp': True})
+resultTwo = test.squaredEuclideanTwo(x, y)
+print(resultTwo)
+
+# stop the timer
+t.stop()
+
+
---
+++ testFunctions.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy.spatial.distance import euclidean
+from scipy.spatial.distance import sqeuclidean
+from sklearn.metrics import pairwise_distances
+
+
+# function to calculate Euclidean distance between two input vectors from scipy
+def euclideanOne(x, y):
+    output = euclidean(x, y)
+    return output
+
+
+# function to calculate Euclidean distances from sklearn
+def euclideanTwo(x, y):
+    output = pairwise_distances()
+    return output
+
+
+# define function for SED
+def squaredEuclideanOne(x, y):
+    output = float(((x[0][0] - y[0][0])**2) + ((x[0][1] - y[0][1])**2))
+    return output
+
+
+def squaredEuclideanTwo(x, y):
+    output = sqeuclidean(x, y)
+    return output
+
+
+# this is a facsimile of the contour comparison function in ultrasoundProbe.py in PATS, adapted for testing purposes.
+# note that intersectionContours is a polydata object in a PATS context, but here it will likely be handled as a
+# list for simplicity's sake
+def objectiveFunction(numPoints, intersectionContours, tracedPointsEpi, tracedPointsEndo):
+    # initialize variables for accruing Euclidean distance
+    totalDistEpi = 0
+    totalDistEndo = 0
+    objective = 10000
+    # loop through each point on the epi
+    return objective
