01195329c238fa4f0c3b55000e78901a34661914
colingibbons <cogibbo@siue.edu>
2022-01-06 15:32:53 -0600

    modified script to better parse different ultrasound scans/feature types

--- objectiveFeatures.py
+++ objectiveFeatures.py
@@ -5,27 +5,39 @@ import os
 # hardcoded path to feature text files
 directory = 'C:\\Users\\cogibbo\\Desktop\\us-mri-fusion-data\\phantomCases\\Feature Analysis\\'
 
-# list of ultrasound planes by offset from base of phantom
-caseList = ['0cm', '1cm', '2cm', '3cm', '4cm', '5cm']
-# actual Z values corresponding with physically realistic probe alignment
-actualValues = [101, 89, 77, 65, 53, 41]
+# list of ultrasound planes by location and "true" Z-offset
+caseDict = {
+    '0cm': 101,
+    '1cm': 89,
+    '2cm': 77,
+    '3cm': 65,
+    '4cm': 53,
+    '5cm': 41
+}
 
 # loop through each text file
 for textFile in os.listdir(directory):
 
     path = f'{directory}{textFile}'
-    data = np.loadtxt(path, skiprows=1, delimiter=', ')
+    file = open(path, 'r')
+    # get the feature type and the ultrasound scan it corresponds with from the first line of the text file
+    featureType, caseLabel = file.readline().split(', ')
+    file.close()
+    caseLabel = caseLabel.replace('\n', '')
+    # load the numerical data into an array
+    data = np.loadtxt(path, skiprows=2, delimiter=', ')
 
-    textFile = textFile.replace('.txt', '')
-
+    # set up plot
     fig = plt.figure()
-    plt.title(f'Feature Correlation vs. Z Value, {textFile}')
-    plt.ylabel('Calculated Feature Value')
+    plt.title(f'{featureType} vs. Z Value, {caseLabel}')
+    plt.ylabel(f'Calculated {featureType} Value')
     plt.xlabel('Z Value')
+    # plot correlation curves for each of the three tissue types
     plt.plot(data[:, 0], data[:, 1], c='red')
     plt.plot(data[:, 0], data[:, 2], c='green')
     plt.plot(data[:, 0], data[:, 3], c='purple')
-    plt.axvline(x=89)
+    # add a vertical axis line corresponding with the "true" Z-location of this US plane relative to MRI model
+    plt.axvline(x=caseDict[caseLabel])
     plt.show()
 
     print('stop')
